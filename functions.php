2cc40e6437a83ca3eeeb37cb4d307acb08366705
cr-walton <cxw507@gmail.com>
2022-08-01 13:38:23 +0100

    pull request changes: readability and changed db request to function

--- functions.php
+++ functions.php
@@ -1,15 +1,28 @@
 <?php
 /**
+ * Database query, fetch all
+ *
+ * @return returns all data from the database `characters`
+ */
+function databaseFetchAll() {
+    $db = new PDO('mysql:host=db; dbname=collection_project', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare("SELECT `charname`, `class`, `level`, `strength`, `dexterity`, `constitution`, `intelligence`, `wisdom`, `charisma` FROM `characters`;");
+    $query->execute();
+    return $query->fetchAll();
+}
+
+/**
  * Takes the data from the database, separates the array into individual characters and then echos
  * out the information
  *
  * @param array $characters
- * @return echos out the details from the character 
+ * @return returns a string of the details from the characters
  */
-function displayCharacters(array $characters) {
+function displayCharacters(array $characters): string {
     if(count($characters) === 0){
         return 'Data is not available';
-    }else {
+    } else {
         $result = '';
         foreach($characters as $character){
             $result .= '<p>Name: ' . $character['charname'] . ' Class: ' . $character['class'] . '</p>';
@@ -17,18 +30,9 @@ function displayCharacters(array $characters) {
             $result .= '<p>Strength: ' . $character['strength'] . '<br>' . 'Dexterity: ' . $character['dexterity'] . '</p>';
             $result .= '<p>Constitution: ' . $character['constitution'] . '<br>' . 'Intelligence: ' . $character['intelligence'] . '</p>';
             $result .= '<p>Wisdom: ' . $character['wisdom'] . '<br>' . 'Charisma: ' . $character['charisma'] . '</p>';
-    }
+        }
     return $result;
     }
 }
-
-
-
-
-
-
-
-
-
 
 ?>
--- project_index.php
+++ project_index.php
@@ -1,17 +1,9 @@
 <?php
 require_once('functions.php');
-$db = new PDO('mysql:host=db; dbname=collection_project', 'root', 'password');
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare("SELECT `charname`, `class`, `level`, `strength`, `dexterity`, `constitution`, `intelligence`, `wisdom`, `charisma` FROM `characters`;");
-// $query->bindParam(':email', $email);
-$query->execute();
-$characters = $query->fetchAll();
+$characters = databaseFetchAll();
 
 echo displayCharacters($characters);
-// echo $characters[0]['charname'];
-echo '<pre>';
-var_dump($characters);
-echo '</pre>';
+
 
 
 ?>
