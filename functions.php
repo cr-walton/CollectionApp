dff06a94fc43336c06b2e5b26077a38fac1d8a27
cr-walton <cxw507@gmail.com>
2022-08-03 11:03:23 +0100

    Resolving conflicts

--- functions.php
+++ functions.php
@@ -40,8 +40,68 @@ function displayCharacters(array $characters): string {
         $result .= '<p>Constitution: ' . $character['constitution'] . '<br>' . 'Intelligence: ' . $character['intelligence'] . '</p>';
         $result .= '<p>Wisdom: ' . $character['wisdom'] . '<br>' . 'Charisma: ' . $character['charisma'] . '</p></section>';
     }
-    return $result;
-    
+
+    return $result; 
+}
+
+/**
+ * Validates the $_POST data to ensure that no fields are empty when inserting
+ *
+ * @param array Data from the form on previous page, stored in $_POST
+ * @return bool returns false if not validated
+ */
+function validateFields(array $post): bool {
+    if((!isset($post['charname']) || $post['charname'] === '') 
+    || (!isset($post['class']) || $post['class'] === '')
+    || (!isset($post['level']) || $post['level'] === '')
+    || (!isset($post['strength']) || $post['strength'] === '')
+    || (!isset($post['dexterity']) || $post['dexterity'] === '')
+    || (!isset($post['constitution']) || $post['constitution'] === '')
+    || (!isset($post['intelligence']) || $post['intelligence'] === '') 
+    || (!isset($post['wisdom']) || $post['wisdom'] === '') 
+    || (!isset($post['charisma']) || $post['charisma'] === '')) {
+        return false;
+    } return true;
+}
+
+/**
+ * Validates the stats to ensure that they are not outside of acceptable range
+ *
+ * @param array data from the form on previous page with $_POST
+ * @return bool returns false if stats are over 20
+ */
+function validateStats(array $post): bool {
+    if($post['strength'] > 20 
+    || $post['dexterity'] > 20 
+    || $post['constitution'] > 20 
+    || $post['intelligence'] > 20 
+    || $post['wisdom'] > 20 
+    || $post['charisma'] > 20) {
+        return false;
+    } return true;
+}
+
+
+/**
+ * Fetches messages from $_GET 
+ *
+ * @return string returns a string from $_GET
+ */
+function getMessage(): string {
+    if(isset($_GET['error'])) {
+        $result = $_GET;
+        return $result['error'];
+    } return '';
+}
+/**
+ * Sanitises any potentially malicious entries before inserting into db
+ *
+ * @return array returns an array containing the sanitised character name and class name
+ */
+function sanitizePost(): array {
+    $charname = filter_var($_POST['charname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $class = filter_var($_POST['class'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    return [$charname, $class];
 }
 
 ?>
--- project_index.php
+++ project_index.php
@@ -7,10 +7,41 @@ $characters = databaseFetchAll($db);
 
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link href="normalize.css" type="text/css" rel="stylesheet" />
+	    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+	    <link href="style.css" type="text/css" rel="stylesheet" />
+		    <title>Chris Walton</title>
     </head>
     <body>
-        <div>
-            <?=displayCharacters($characters);?>
-        </div>
+        <nav>
+            <div class='nav-links'>
+                <a href='char_creation.php'>Create a new character</a>
+                <a href="#contact-me">Contact</a>
+            </div>
+            <div class='nav-contact'>
+                <a href="https://cr-walton.github.io/">Portfolio</a>
+                <a href="crwaltoncv.docx" id="cv">Download CV</a>
+            </div>
+        </nav>
+        <div class='project'>
+            <div class='collection-cont'>
+                <h1>D&D Character Collection</h1>
+                <?=displayCharacters($characters);?>
+            </div>
+            <section class='contact-container' id='contact-me'>
+            <div class='contact-links'>
+                <a href='mailto:walton-cr@proton.me'><img src='images/email-logo.png' alt='email Link'></a>
+                <a href='#'><img src='images/LinkedIn-logo.png' alt='LinkedIn Link'></a>
+                <a href='https://github.com/cr-walton'><img src='images/GitHub-Mark-32px.png' alt='GitHub Link'></a>
+            </div>
+        </div>	
+	</section>
+	
+	<footer>
+		<div class='foot-cont'>
+			<p>&#169; Copyright 2022</p>
+		</div>
+	</footer>
     </body>
 </html>
